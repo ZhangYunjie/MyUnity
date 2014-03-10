da973548834a815c02f29bc580f210a0abb19a17
zhang_yunjie <achillessatan@gmail.com>
2014-03-10 18:46:23 +0900

    Create a scene, player and its enemies

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Simply moves the current game object
+/// </summary>
+public class MoveScript : MonoBehaviour
+{
+	// 1 - Designer variables
+
+	/// <summary>
+	/// Object speed
+	/// </summary>
+	public Vector2 speed = new Vector2 (10, 10);
+
+	/// <summary>
+	/// Moving direction
+	/// </summary>
+	public Vector2 direction = new Vector2(-1, 00);
+
+	private Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// 2 - Movement
+		movement = new Vector2 (speed.x * direction.x, speed.y * direction.y);
+	}
+
+	void FixedUpdate()
+	{
+		// Apply movement to the rigidbody
+		rigidbody2D.velocity = movement;
+	}
+}
