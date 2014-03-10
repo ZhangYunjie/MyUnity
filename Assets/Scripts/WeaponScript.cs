2ceda906d1c820e1b6c0cbaee51578454df2d19c
yunjie.zhang <yunjie.zhang@drecom.co.jp>
2014-03-10 23:56:50 +0900

    z-order modification

---
+++ Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Launch projectile
+/// </summary>
+public class NewBehaviourScript : MonoBehaviour {
+
+	//--------------------------------
+	// 1 - Designer variables
+	//--------------------------------
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
