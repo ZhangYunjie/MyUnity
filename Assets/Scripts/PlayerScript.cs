3bd4432f97bc102c7d75a3ff2425f43a24b3ef06
zhang_yunjie <achillessatan@gmail.com>
2014-03-11 11:13:17 +0900

    Complete the shot

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -28,6 +28,17 @@ public class PlayerScript : MonoBehaviour
 
 		// 4 - Movement per direction
 		movement = new Vector2 (speed.x * inputX, speed.y * inputY);
+
+		// 5 - Shooting
+		bool shoot = Input.GetButtonDown ("Fire1");
+		shoot |= Input.GetButtonDown ("Fire2");
+
+		if (shoot) {
+						WeaponScript weapon = GetComponent<WeaponScript> ();
+						if (weapon != null) {
+								weapon.Attack (false);
+						}
+				}
 	}
 
 	void FixedUpdate()
