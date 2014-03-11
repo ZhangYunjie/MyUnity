6c13f03a4962d6a29cbd0add8e01b5d9cad50c64
zhang_yunjie <achillessatan@gmail.com>
2014-03-11 17:25:28 +0900

    Add the sound effect

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -5,19 +5,70 @@
 /// </summary>
 public class EnemyScript : MonoBehaviour {
 
+	private bool hasSpawn;
+	private MoveScript moveScript;
 	private WeaponScript[] weapons;
 
 	void Awake() {
-				// Retrieve the weapon only once
-				weapons = GetComponentsInChildren<WeaponScript> ();
+		// Retrieve the weapon only once
+		weapons = GetComponentsInChildren<WeaponScript> ();
+
+		// Retrieve scripts to disable when not spawn
+		moveScript = GetComponent<MoveScript> ();
+	}
+
+	// 1 - Disable everything
+	void Start()
+	{
+		hasSpawn = false;
+
+		//Disable everything
+		// -- collider
+		collider2D.enabled = false;
+		// -- Moving
+		moveScript.enabled = false;
+		// -- Shooting
+		foreach (WeaponScript weapon in weapons) {
+				weapon.enabled = false;
 		}
+	}
 
 	void Update () {
-				foreach (WeaponScript weapon in weapons) {
-						// Auto-fire
-						if (weapon != null && weapon.CanAttack) {
-								weapon.Attack (true);
+				// 2 - Check if the enemy has spawned.
+				if (hasSpawn == false) {
+						if (renderer.IsVisibleForm (Camera.main)) {
+								Spawn ();
+						}
+				} else {
+						foreach (WeaponScript weapon in weapons) {
+								// Auto-fire
+								if (weapon != null && weapon.CanAttack) {
+										weapon.Attack (true);
+
+										SoundEffectsHelper.Instance.MakeEnemyShotSound();
+								}
+						}
+						// 4 - Out of the camera ? Destroy the game object.
+						if (renderer.IsVisibleForm (Camera.main) == false) {
+								Destroy (gameObject);
 						}
 				}
+
 		}
+
+	// 3 - Activate itself.
+	private void Spawn()
+	{
+		hasSpawn = true;
+
+		// Enable everything
+		// -- Collider
+		collider2D.enabled = true;
+		// -- Moving
+		moveScript.enabled = true;
+		// -- Shooting
+		foreach (WeaponScript weapon in weapons) {
+				weapon.enabled = true;
+		}
+	}
 }
